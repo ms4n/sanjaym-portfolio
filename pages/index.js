3e5a2932432b10a4b1966447490697f4382859d9
Sanjay M <arjunsanjay0@gmail.com>
2022-07-30 05:44:41 +0530

    simple page for deployment

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,8 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="my-10 font-bold text-center m-auto text-4xl text-blue-600">
-      <a className="text-black">Welcome to</a> Sanjay M's{" "}
-      <a className="text-black">Portfolio.</a>
+    <div className="h-screen bg-main-color font-bold flex justify-center items-center m-auto text-8xl text-gray-800">
+      Sanjay M
     </div>
   );
 }
