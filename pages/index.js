433df8bb250432c1ad7b4bf8fa034a72205366d0
Sanjay M <arjunsanjay0@gmail.com>
2022-07-30 05:51:29 +0530

    set dev as deployment branch

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-main-color font-bold flex justify-center items-center m-auto text-8xl text-gray-800">
+    <div className="h-screen bg-main-color font-semibold flex justify-center items-center m-auto text-8xl text-gray-800">
       Sanjay M
     </div>
   );
