8fbdae4f7a1e16b4fca3f9acb0a1616888ca2c54
Sanjay M <arjunsanjay0@gmail.com>
2022-11-19 09:23:43 +0530

    added font

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
             opacity: 0,
           },
           visible: {
-            scale: 1,
+            scale: 1.5,
             opacity: 1,
             transition: {
               delay: 0.5,
@@ -20,30 +20,10 @@ export default function Home() {
           },
         }}
       >
-        <h1 className="font-semibold text-2xl lg:text-8xl text-gray-800">
+        <h1 className="font-playfair tracking-tight font-regular text-2xl lg:text-6xl text-gray-800">
           Sanjay M
         </h1>
       </motion.div>
-
-      <div className="h-14 md:h-20 text-sm md:text-normal font-mono flex justify-center items-center fixed bottom-0 w-screen bg-gray-800 text-gray-100">
-        <div className="relative">
-          <div className="py-12 animate-marquee whitespace-nowrap">
-            <span className="mx-1">The page is under design maintenance *</span>
-            <span className="mx-1">The page is under design maintenance *</span>
-            <span className="mx-1">The page is under design maintenance *</span>
-            <span className="mx-1">The page is under design maintenance *</span>
-            <span className="mx-1">The page is under design maintenance *</span>
-          </div>
-
-          <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
-            <span className="mx-1">The page is under design maintenance *</span>
-            <span className="mx-1">The page is under design maintenance *</span>
-            <span className="mx-1">The page is under design maintenance *</span>
-            <span className="mx-1">The page is under design maintenance *</span>
-            <span className="mx-1">The page is under design maintenance *</span>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
