8e5489303d9bae039cb824b11c1bee6656f27888
Sanjay M <arjunsanjay0@gmail.com>
2022-12-02 15:14:20 +0530

    added header and satoshi font

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,84 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-main-color ">
+    <div className="h-full bg-main-color ">
       <div className="px-10 py-8">
         <p className="text-lg font-playfair">Sanjay M</p>
-        <p className="text-xs font-inter tracking-tight opacity-50">
+        <p className="text-xs font-inter tracking-tight opacity-50 ">
           Bengaluru, India
         </p>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 p-10 gap-32">
+        <div>
+          <p className="">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. In
+            fermentum posuere urna nec tincidunt praesent. Quam nulla porttitor
+            massa id neque aliquam. Dolor magna eget est lorem ipsum dolor sit
+            amet consectetur. Auctor eu augue ut lectus. Accumsan sit amet nulla
+            facilisi morbi. Accumsan tortor posuere ac ut consequat semper
+            viverra. Nec ultrices dui sapien eget mi proin sed libero. Faucibus
+            nisl tincidunt eget nullam non nisi est sit. Ante metus dictum at
+            tempor commodo. Mi quis hendrerit dolor magna eget. Ultrices
+            tincidunt arcu non sodales neque sodales ut. grid first column think
+            what information can go here Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. In fermentum posuere urna nec tincidunt
+            praesent. Quam nulla porttitor massa id neque aliquam. Dolor magna
+            eget est lorem ipsum dolor sit amet consectetur. Auctor eu augue ut
+            lectus. Accumsan sit amet nulla facilisi morbi. Accumsan tortor
+            posuere ac ut consequat semper viverra. Nec ultrices dui sapien eget
+            mi proin sed libero. Faucibus nisl tincidunt eget nullam non nisi
+            est sit. Ante metus dictum at tempor commodo. Mi quis hendrerit
+            dolor magna eget. Ultrices tincidunt arcu non sodales neque sodales
+            ut. grid first column think what information can go here Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. In fermentum posuere
+            urna nec tincidunt praesent. Quam nulla porttitor massa id neque
+            aliquam. Dolor magna eget est lorem ipsum dolor sit amet
+            consectetur. Auctor eu augue ut lectus. Accumsan sit amet nulla
+            facilisi morbi. Accumsan tortor posuere ac ut consequat semper
+            viverra. Nec ultrices dui sapien eget mi proin sed libero. Faucibus
+            nisl tincidunt eget nullam non nisi est sit. Ante metus dictum at
+            tempor commodo. Mi quis hendrerit dolor magna eget. Ultrices
+            tincidunt arcu non sodales neque sodales ut. grid first column think
+            what information can go here Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. In fermentum posuere urna nec tincidunt
+            praesent. Quam nulla porttitor massa id neque aliquam. Dolor magna
+            eget est lorem ipsum dolor sit amet consectetur. Auctor eu augue ut
+            lectus. Accumsan sit amet nulla facilisi morbi. Accumsan tortor
+            posuere ac ut consequat semper viverra. Nec ultrices dui sapien eget
+            mi proin sed libero. Faucibus nisl tincidunt eget nullam non nisi
+            est sit. Ante metus dictum at tempor commodo. Mi quis hendrerit
+            dolor magna eget. Ultrices tincidunt arcu non sodales neque sodales
+            ut.Accumsan tortor posuere ac ut consequat semper viverra. Nec
+            ultrices dui sapien eget mi proin sed libero. Faucibus nisl
+            tincidunt eget nullam non nisi est sit. Ante metus dictum at tempor
+            commodo. Mi quis hendrerit dolor magna eget. Ultrices tincidunt arcu
+            non sodales neque sodales ut. grid first column think what
+            information can go here Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. In fermentum posuere urna nec tincidunt
+            praesent. Quam nulla porttitor massa id neque aliquam. Dolor magna
+            eget est lorem ipsum dolor sit amet consectetur. Auctor eu augue ut
+            lectus. Accumsan sit amet nulla facilisi morbi. Accumsan tortor
+            posuere ac ut consequat semper viverra. Nec ultrices dui sapien eget
+            mi proin sed libero. Faucibus nisl tincidunt eget nullam non nisi
+            est sit. Ante metus dictum at tempor commodo. Mi quis hendrerit
+            dolor magna eget. Ultrices tincidunt arcu non sodales neque sodales
+            ut.
+          </p>
+        </div>
+
+        <div className="">
+          <p className="font-playfair text-6xl">a developer with a</p>
+          <p className="font-playfair text-6xl">love for design.</p>
+          <p className="font-satoshir text-xl mt-4">
+            + dolor sit amet, consectetur adipiscing elit, sed do
+          </p>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
       fontFamily: {
         playfair: ["Playfair Display", "serif"],
         inter: ["Inter", "sans-serif"],
+        satoshib: ["Satoshi-Bold", "sans-serif"],
+        satoshil: ["Satoshi-Light", "sans-serif"],
+        satoshim: ["Satoshi-Medium", "sans-serif"],
+        satoshir: ["Satoshi-Regular", "sans-serif"],
       },
       colors: {
         "main-color": "#CCCCFF",
